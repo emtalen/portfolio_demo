db88d0e3e37c1735c13581abd2bb8c836ff4897f
emtalen <59577366+emtalen@users.noreply.github.com>
2021-01-28 11:22:57 +0100

    adding redirect config to build script

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -5,7 +5,7 @@ import { Container } from "semantic-ui-react";
 const Hello = () => {
   return (
     <Container >
-      <h1 id="hello">Hello World</h1>
+      <h1 class="hello">Hello World</h1>
     </Container>
   );
 };
